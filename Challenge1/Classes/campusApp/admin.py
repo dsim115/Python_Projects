f7a0ee4d7cd139c20bfd2f06c04afc63bf55e135
David Simental <davidjsimental@gmail.com>
2025-12-29 13:42:39 -0700

    Delete Challenge1 directory

--- Challenge1/Classes/campusApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UniversityCampus
-
-# Register the UniversityCampus model so it appears in the admin interface
-admin.site.register(UniversityCampus)
